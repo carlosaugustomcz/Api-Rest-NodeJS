31f0caead1e76f081b0aadad08bfa9eb4e4341b4
carlos.augustomcz <carlos.augustomcz@gmail.com>
2023-02-28 09:12:12 -0300

    Create routes, POST, GET

--- db/migrations/20230217115502_create-transactions.ts
+++ db/migrations/20230217115502_create-transactions.ts
@@ -1,10 +1,20 @@
+import { table } from "console";
 import { Knex } from "knex";
+import { SCHEMA } from "sqlite3";
 
 
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('transactions', (table) => {
+        table.uuid('id').primary()
+        table.text('title').notNullable()
+        table.decimal('amount', 10, 2).notNullable()
+        table.timestamp('create_at').defaultTo(knex.fn.now()).notNullable
+
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+   await knex.schema.dropTable('transactions') 
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import fastify from 'fastify'
+// import { knex } from './database'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', () =>{
-    return 'Hello World'
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
 })
 
+
+
 app.listen({
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
     console.log('HTTP Server Running!')
 })
